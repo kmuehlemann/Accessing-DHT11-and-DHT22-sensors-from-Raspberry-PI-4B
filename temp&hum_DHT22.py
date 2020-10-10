e7765010c87cb6b6bed60350bdab08ca55e15929
kmuehlemann <66792545+kmuehlemann@users.noreply.github.com>
2020-10-10 11:41:23 +0200

    Add files via upload

---
+++ temp&hum_DHT22.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import time
+import RPi.GPIO as GPIO
+import bus_access_DHT22
+
+def main():
+    GPIO.setmode(GPIO.BCM)
+    sigPort = 18
+    try:
+        readsensor = bus_access_DHT22.Readsensor(sigPort)
+        time.sleep(1)
+        while True:
+            temp, hum = readsensor.temp()
+            if hum < 0: # A negative humidity value is code for an error
+                print(readsensor.errorMsg(hum))
+            else:
+                print("Temp: "+str(temp)+"°C  |  Rel. Humidity: "+str(hum)+"%")
+            time.sleep(0.5) # 1.5s wait in readsensor.temp(), i.e. 2s between calls as specified
+    except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
+        print("Cleaning up")
+        GPIO.cleanup()
+
+if __name__ == '__main__':
+    main()
